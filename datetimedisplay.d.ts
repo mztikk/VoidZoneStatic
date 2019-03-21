6a7b38c408081f6b26da2e93eeed870008b8744b
mztikk <nevigskcuforez@gmail.com>
2019-03-21 17:10:22 +0100

    changed time display to 2-digit for everything except year

--- datetimedisplay.d.ts
+++ datetimedisplay.d.ts
@@ -1,2 +1,10 @@
+declare const dateoptions: {
+    day: string;
+    month: string;
+    year: string;
+    hour: string;
+    minute: string;
+    second: string;
+};
 declare function dateTimeParse(str: string): Date;
 declare function updateDivDisplays(): void;
--- datetimedisplay.js
+++ datetimedisplay.js
@@ -1,4 +1,12 @@
 "use strict";
+var dateoptions = {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+};
 function dateTimeParse(str) {
     return new Date(str.slice(0, 4) + "-" + str.slice(4, 6) + "-" + str.slice(6, 8) + "T" + str.slice(9, 11) + ":" + str.slice(11, 13) + ":" + str.slice(13, 15));
 }
@@ -6,7 +14,7 @@ function updateDivDisplays() {
     var divs = document.querySelectorAll('div.box a');
     for (var i = 0; i < divs.length; i++) {
         var element = divs[i];
-        element.innerHTML = dateTimeParse(element.innerHTML).toLocaleString();
+        element.innerHTML = dateTimeParse(element.innerHTML).toLocaleDateString("de-DE", dateoptions);
     }
 }
 window.onload = function () {
